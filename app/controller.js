f3ebf6dcc9d98b5c8cee94b46a9fe348d666e1ca
dastangoo <hamed.dastangoo@gmail.com>
2017-11-18 19:59:37 +0330

    Filters

--- app/controller.js
+++ app/controller.js
@@ -1,10 +1,10 @@
 var con2 = angular.module("con2", []);
 con2.controller('newCtrl2', function($scope) {
   $scope.people = [
-    {name: 'John', age: '21', skill: 'angularjs'},
-    {name: 'Mark', age: '24', skill: 'Bootstrap'},
-    {name: 'Joe', age: '32', skill: 'PHP'},
-    {name: 'Garry', age: '35', skill: 'HTML5'},
-    {name: 'Jackson', age: '20', skill: 'JavaScript'}
+    {name: 'John', age: '21', skill: 'angularjs', salary: 5000},
+    {name: 'Mark', age: '24', skill: 'Bootstrap', salary: 10000},
+    {name: 'Joe', age: '32', skill: 'PHP', salary: 8000},
+    {name: 'Garry', age: '35', skill: 'HTML5', salary: 6000},
+    {name: 'Jackson', age: '20', skill: 'JavaScript', salary: 4000}
   ];
 });
